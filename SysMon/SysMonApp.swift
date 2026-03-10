b9c6cddaef75b4e0648dbd08aff4a933f009207a
vivek <vivekkrishnan1729@gmail.com>
2026-03-11 00:13:58 +0530

    Initial Commit

---
+++ SysMon/SysMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SysMonApp.swift
+//  SysMon
+//
+//  Created by Vivek Krishnan on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SysMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
